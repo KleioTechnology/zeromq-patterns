875548db8caa5a1c841b6368ad2f3c8559b6c3a3
Nelson Vega <nelson@kleiotechnology.com>
2022-05-06 15:13:55 -0400

    first commit -errors

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# zeromq-patterns
+# Zero MQ Patterns 
+
+## Implementation : NETMQ
+
+   1. Dealer
+   2. Majordomo
+   3. Publisher-subscriber
+   4. Ruter
---
+++ majordomo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace majordomo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Majordomo!");
+            using var dealer=new DealerSocket();
+            dealer.Connect("tcp:127.0.0.1:5555");
+
+            dealer.SendFrame("A new message");
+        }
+    }
+}
